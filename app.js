3576730aa070d5278e1e6998adbd8968e87bd523
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-03-12 13:37:15 -0700

    initial scaffolding and objects

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+
+var firstAndPike = {
+    location: '1st and Pike',
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+}
+
+var seaTacAirport = {
+    location: 'SeaTac Airport',
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgCookieSale: 1.2,
+}
+
+var seattleCenter = {
+    location: 'Seattle Center',
+    minCustomer: 11,
+    maxCustomer: 38,
+    avgCookieSale: 3.7,
+}
+
+var capitolHill = {
+    location: 'Capitol Hill',
+    minCustomer: 20,
+    maxCustomer: 38,
+    avgCookieSale: 2.3,
+}
+
+var alki = {
+    location: 'Alki',
+    minCustomer: 2,
+    maxCustomer: 16,
+    avgCookieSale: 4.6,
+}
