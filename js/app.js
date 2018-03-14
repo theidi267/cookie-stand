21f9dd5e36303f2fc782dbd85831778d47cdd5c1
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-03-13 17:40:48 -0700

    Houston, we have a table

--- js/app.js
+++ js/app.js
@@ -1,77 +1,89 @@
-'use strict'
+'use strict';
 
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
-
-Locations.prototype.custPerHour = function getRandomInt(){
-    return Math.round(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer; 
-};
-
-Locations.prototype.cookiesPerHr = function(){
-    for(var i=0; i < storeHours.length; i++) {
-        
-        var doTheMath = Math.round(this.custPerHr() * this.avgCookieSale);
-
-        this.totalPerHr.push(doTheMath);
-        this.total +=  doTheMath;                   
-    }    
- };
-
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var allLocations = []; //this holds all the objects
 
-var figuresTable = document.getElementById("finalfigures"); //accsesses the table
+var figuresTable = document.getElementById('finalfigures'); //accsesses the table
 
 // We need a constructor to make our dog objects
 
-function Locations(location, minCustomer, maxCustomer, avgCookieSale,){
-    this.location = location;
-    this.minCustomer = minCustomer;
-    this.maxCustomer = maxCustomer;
-    this.avgCookieSale = avgCookieSale;
-    this.totalPerHr = [];
-    this.total = 0;
-    allLocations.push(this);
-}            //constructs the element an pushes it in the array
+function Locations(location, minCustomer, maxCustomer, avgCookieSale){
+  this.location = location;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSale = avgCookieSale;
+  this.totalPerHr = [];
+  this.total = 0;
+  allLocations.push(this);
+} //constructs the element an pushes it in the array
 
-Locations.prototype.custPerHour = function getRandomInt(){
-    return Math.round(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer; 
+Locations.prototype.custPerHr = function getRandomInt(){
+  return Math.round(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
 };
 
 Locations.prototype.cookiesPerHr = function(){
-    for(var i=0; i < storeHours.length; i++) {
-        
-        var doTheMath = Math.round(this.custPerHr() * this.avgCookieSale);
+  for(var i=0; i < storeHours.length; i++) {
 
-        this.totalPerHr.push(doTheMath);
-        this.total +=  doTheMath;   
-                   
-    }    
- };           //these do the math
+    var doTheMath = Math.round(this.custPerHr() * this.avgCookieSale);
 
+    this.totalPerHr.push(doTheMath);
+    this.total += doTheMath;
+  }
+};
 
 Locations.prototype.render = function () {
 
-  var trElement = document.createElement("tr");
-  var tdElement = document.createElement("td");
+  this.cookiesPerHr();
+
+  var trElement = document.createElement('tr');
+  var tdElement = document.createElement('td');
 
   tdElement.textContent = this.location;
   trElement.appendChild(tdElement);
 
-  tdElement = document.createElement("td");
-  tdElement.textContent = this.totalPerHr[0];
-  trElement.appendChild(tdElement);
+  for(var i = 0; i < this.totalPerHr.length; i++){
 
-  // append tr to table
+    tdElement = document.createElement('td');
+    tdElement.textContent = this.totalPerHr[i];
+    trElement.appendChild(tdElement);
+  }
 
-  something.appendChild(trElement);
+  figuresTable.appendChild(trElement);
 
 };
 
-var pike = new Locations('1st and Pike', 23, 65, 6.3);    //created the objects
+function heading(){
+
+  var trHead = document.createElement('tr');
+  var thHead = document.createElement('th');
+  thHead.textContent = 'Locations';
+  trHead.appendChild(thHead);
+
+  for(var i = 0; i < storeHours.length; i++) {
+
+    thHead = document.createElement('th');
+    thHead.textContent = storeHours[i];
+    trHead.appendChild(thHead);
+
+  }
+  figuresTable.appendChild(trHead);
+}
+
+
+var pike = new Locations('1st and Pike', 23, 65, 6.3); //created the objects
 var seatac = new Locations('SeaTac Airport', 3, 24, 1.2);
-var center = new Location('Seattle Center', 11, 38, 3.7);
-var capitolhill = new Location('Capitil Hill', 20, 38, 2.3);
-var alki = new Location('Alki', 2, 16, 4.6)        
+var center = new Locations('Seattle Center', 11, 38, 3.7);
+var capitolhill = new Locations('Capitil Hill', 20, 38, 2.3);
+var alki = new Locations('Alki', 2, 16, 4.6);
+
+heading();
+pike.render();
+seatac.render();
+center.render();
+capitolhill.render();
+alki.render();
+
 
 
 // We need a separate function to make the table header
@@ -96,7 +108,7 @@ var alki = new Location('Alki', 2, 16, 4.6)
 
 // var hourlyCount = document.createElement('p');
 //         hourlyCount.textContent ='Total for the day: ' + this.total;
-        
+
 //     var totalCount = document.getElementById('pike');
 //         totalCount.appendChild(hourlyCount)
 
