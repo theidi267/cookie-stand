fd4cfdf49abe05facedbc488b6f6c82bd66b5ccf
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-03-17 15:53:51 -0700

    fish is now a link, buttons in place

--- main.js
+++ main.js
@@ -18,3 +18,21 @@ function openAboutUs() {
     document.getElementById('us').hidden = true;
   }
 }
+
+var whatToExpect = document.getElementById('expect');
+
+whatToExpect.addEventListener('click', openWhatToExpect);
+
+function openWhatToExpect() {
+
+  if (whatToExpect.hidden === true) {
+
+    document.getElementById('expect').hidden = false;
+
+  }
+
+  else {
+
+    document.getElementById('expect').hidden = true;
+  }
+}
